87a034227c8b43da670a6df6a2c9031b2c7d28c4
Arsen <abdigal.2015@gmail.com>
2022-04-18 08:30:49 +0600

    frontend handles quiz details page if the quiz was passed by the current user

--- QuizApp.WebUI/src/components/Quiz.jsx
+++ QuizApp.WebUI/src/components/Quiz.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Container, Alert } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
-import { getQuiz, isAuthenticated, isQuizCompletedByCurrentUser } from './api';
+import { getQuiz, isAuthenticated } from './api';
 
 export const Quiz = () => {
     const [quiz, setQuiz] = useState({});
@@ -13,22 +13,15 @@ export const Quiz = () => {
     useEffect(() => {
         getQuiz(quizid).then(res => setQuiz(res.data));
         isAuthenticated().then(res => setIsAuth(res.data));
-        isQuizCompletedByCurrentUser(quizid).then(res => setIsCompleted(res.data));
+        setIsCompleted(quizid).then(res => setIsCompleted(res.data));
     }, []);
 
     let quizAccess = <></>;
     if (isAuth === false)
-        quizAccess = <Alert variant="primary">
+        quizAccess = <Alert variant="danger" dismissible>
             <Alert.Heading>You aren't authorized!</Alert.Heading>
             <p>You cannot start quiz if you aren't authorized. You can Login or create a new account by clicking on menu dropdown on the top right angle!</p>
-        </Alert>;
-    else if (isCompleted === true)
-        quizAccess = <Alert variant="info">
-            <Alert.Heading>You have already passed this quiz..</Alert.Heading>
-            <p>You cannot repass the quiz by yourself. To do so, please, contant the administrators.</p>
-        </Alert>;
-    else quizAccess = <Link to='questions'>Start Quiz</Link>
-
+        </Alert>
     return (
         <Container style={{ marginTop: "100px" }}>
             <p>Title:       {quiz.title}</p>
