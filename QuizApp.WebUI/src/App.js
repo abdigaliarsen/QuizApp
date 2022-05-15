968de996c8cb888f9d05757beae6652204204f76
Arsen <abdigal.2015@gmail.com>
2022-05-15 08:12:49 +0600

    profile route is added

--- QuizApp.WebUI/src/App.js
+++ QuizApp.WebUI/src/App.js
@@ -4,6 +4,7 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { Quiz } from './components/Quiz';
 import { Question } from './components/Question';
+import { Profile } from './components/profile';
 
 export const App = () => {
     return (
@@ -13,6 +14,7 @@ export const App = () => {
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/quiz/:quizid" element={<Quiz />} />
                 <Route exact path="/quiz/:quizid/questions/" element={<Question />} />
+                <Route exact path="/profile" element={<Profile />} />
             </Routes>
         </BrowserRouter>
     );
